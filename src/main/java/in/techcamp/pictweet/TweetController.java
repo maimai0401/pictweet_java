39def46931df69f18661dfa376ef5668e70ea882
higuchimai <64880970+maimai0401@users.noreply.github.com>
2024-02-02 15:39:46 +0900

    コントローラー修正

--- src/main/java/in/techcamp/pictweet/TweetController.java
+++ src/main/java/in/techcamp/pictweet/TweetController.java
@@ -70,6 +70,62 @@ public class TweetController {
 
         return "detail";
     }
+
+    @GetMapping("/user/{userId}/tweet/{tweetId}/edit")
+    public String edit(@PathVariable("tweetId") Integer tweetId, Model model) {
+
+        TweetEntity tweet;
+
+        try {
+            tweet = tweetRepository.findById(tweetId)
+                    .orElseThrow(() -> new EntityNotFoundException("Tweet not found: " + tweetId));
+        } catch (EntityNotFoundException ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return "error";
+        }
+
+        model.addAttribute("tweet", tweet);
+
+        return "edit";
+    }
+
+    @PostMapping("/user/{userId}/tweet/{tweetId}/update")
+    public String update(Authentication authentication,
+                         @ModelAttribute("tweet") TweetEntity tweetEntity,
+                         @PathVariable("userId") Integer userId,
+                         @PathVariable("tweetId") Integer tweetId,
+                         Model model
+    ) {
+
+        String username = authentication.getName();
+
+        TweetEntity tweet;
+        UserEntity user;
+
+        try {
+            tweet = tweetRepository.findById(tweetId).orElseThrow(() -> new EntityNotFoundException("Tweet not found: " + tweetId));
+            user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("User not found: " + userId));
+        } catch (EntityNotFoundException ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return "error";
+        }
+
+        if (user.getUsername().equals(username)) {
+            try {
+                tweet.setName(tweetEntity.getName());
+                tweet.setContent(tweetEntity.getContent());
+
+                tweetRepository.save(tweet);
+            } catch (Exception e) {
+                model.addAttribute("errorMessage", e.getMessage());
+                return "error";
+            }
+        } else {
+            model.addAttribute("errorMessage", "イシューの投稿者と一致しません。");
+            return "error";
+        }
+        return "redirect:/tweet/" + tweetId;
+    }
 }
 
 
