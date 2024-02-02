00c09e8a268d69a6bb82927523e2dea3f2a626bc
higuchimai <64880970+maimai0401@users.noreply.github.com>
2024-02-02 17:38:52 +0900

    リポジトリ修正

---
+++ src/main/java/in/techcamp/pictweet/CommentRepository.java
@@ -0,0 +1,10 @@
+package in.techcamp.pictweet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
+    List<CommentEntity> findByTweet_id(Integer tweetId);
+
+}
