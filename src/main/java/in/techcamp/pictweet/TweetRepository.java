5e0a94e12f24329e015a7aa1d8b9cb9d4b2671e8
higuchimai <64880970+maimai0401@users.noreply.github.com>
2024-02-08 21:40:51 +0900

    サービスクラスの作成/コントローラーの変更

--- src/main/java/in/techcamp/pictweet/TweetRepository.java
+++ src/main/java/in/techcamp/pictweet/TweetRepository.java
@@ -1,7 +1,9 @@
 package in.techcamp.pictweet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface TweetRepository extends JpaRepository<TweetEntity, Integer> {
-
+    // ユーザーIDに基づいて投稿を取得するメソッド
+    List<TweetEntity> findByUserId(Integer userId);
 }
