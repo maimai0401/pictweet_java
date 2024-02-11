11eb52790ca375b8cdf5fb341fd95544cb9ca486
higuchimai <64880970+maimai0401@users.noreply.github.com>
2024-02-11 20:14:26 +0900

    検索メソッド作成

--- src/main/java/in/techcamp/pictweet/TweetController.java
+++ src/main/java/in/techcamp/pictweet/TweetController.java
@@ -6,10 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -164,6 +161,18 @@ public class TweetController {
             return "error";
         }
         return "redirect:/";
+    }
+
+    @GetMapping("/search")
+    public String searchTweets(@RequestParam(name = "keyword", required = false) String keyword, Model model) {
+        List<TweetEntity> tweetList;
+        if (keyword != null && !keyword.isEmpty()) {
+            tweetList = tweetRepository.findByContentContaining(keyword);
+        } else {
+            tweetList = tweetRepository.findAll();
+        }
+        model.addAttribute("tweetList", tweetList);
+        return "search"; // 検索結果を表示するビューページ
     }
 }
 
